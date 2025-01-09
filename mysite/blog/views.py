0cdbf8bacdd13efae0b50ea73e567350370ffa54
Hendry <hduarteyt@gmail.com>
2025-01-09 10:55:54 -0300

    Creating a view to list all the post based on class with the generic class ListView provided by django

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
+from django.views.generic import ListView
 # Create your views here.
+"""
 def post_list(request):
     post_lists = Post.published.all()
     paginator = Paginator(post_lists,3)
@@ -14,6 +16,14 @@ def post_list(request):
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
     return render(request, "blog/post/list.html", {"posts": posts, "page_obj": posts})
+"""
+
+class PostListViews(ListView):
+    """ alternative post list base in class with the generic class ListView provided by django"""
+    queryset = Post.published.all()
+    template_name = "blog/post/list.html"
+    context_object_name = 'posts'
+    paginate_by = 3
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
