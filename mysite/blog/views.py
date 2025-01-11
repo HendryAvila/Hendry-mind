d7beed7455e60ab7278d12cfe95d52ec8550e0bd
Hendry <hduarteyt@gmail.com>
2025-01-11 16:05:23 -0300

    adding the templates for the comment system

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,8 +3,9 @@ from django.http import Http404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentsForm
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 # Create your views here.
 """
 def post_list(request):
@@ -34,7 +35,29 @@ def post_detail(request, year, month, day, post):
                             publish__year=year,
                             publish__month=month,
                             publish__day=day)
-    return render(request, "blog/post/detail.html", {"post": post})
+    comments = post.comments.filter(active=True)
+    form = CommentsForm()
+    return render(request, "blog/post/detail.html", {"post": post,
+                                                    "comments": comments,
+                                                    "form":form
+                                                    })
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post,
+                            id=post_id,
+                            status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentsForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, "blog/post/comment.html",
+                {
+                "post": post,
+                "form": form,
+                "comment": comment
+                })
 
 def post_share(request, post_id):
     # Retrieve post by id
