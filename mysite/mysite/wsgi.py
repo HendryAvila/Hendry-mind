9358de6ced754707c09cd0851fdb5ab3f2f7fc5b
Hendry <hduarteyt@gmail.com>
2025-01-25 15:42:45 -0300

    deleting the code were i create the superuser by code

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -16,13 +16,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
 
-# En views.py o wsgi.py
 
-# Crear superusuario si no existe
-User = get_user_model()
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser(
-        username=os.environ.get('DJANGO_SUPERUSER_USERNAME'),
-        email=os.environ.get('DJANGO_SUPERUSER_EMAIL'),
-        password=os.environ.get('DJANGO_SUPERUSER_PASSWORD')
-    )
+
