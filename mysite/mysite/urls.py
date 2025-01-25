d902a91d173f233335b6b5ec9b32ec1177bc8e1a
Hendry <hduarteyt@gmail.com>
2025-01-25 01:37:19 -0300

    fixing some configuration error

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -8,6 +8,6 @@ sitemaps = {'posts': PostSitemaps}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("blog/", include("blog.urls", namespace="blog")),
+    path("", include("blog.urls", namespace="blog")),
     path("sitemap.xml", sitemap, {'sitemaps': sitemaps,}, name='django.contrib.sitemaps.views.sitemap')
 ]
