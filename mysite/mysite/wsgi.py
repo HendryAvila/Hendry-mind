0cad01e9f6abc096b63fc17a396dc731f9363d1a
Hendry <hduarteyt@gmail.com>
2025-01-25 15:37:52 -0300

    creating a superuser by code 'THIS SHOULD NO BE DONE IN A REAL PROJECT'

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -7,6 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
+from django.contrib.auth import get_user_model
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +15,14 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
+
+# En views.py o wsgi.py
+
+# Crear superusuario si no existe
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser(
+        username=os.environ.get('DJANGO_SUPERUSER_USERNAME'),
+        email=os.environ.get('DJANGO_SUPERUSER_EMAIL'),
+        password=os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+    )
