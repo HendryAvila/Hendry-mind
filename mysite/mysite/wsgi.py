db02f65bc630c2ae0b4ce3943befab70c1fa8546
Hendry <hduarteyt@gmail.com>
2025-02-10 13:58:46 -0300

    changing the config to prod

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -12,7 +12,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings.production')
 
 application = get_wsgi_application()
 
